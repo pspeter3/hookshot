5912c9e649b6bb9c34fc24d40c248787a2182974
Phips Peter <pspeter333@gmail.com>
2014-12-11 21:36:17 -0800

    Adding the ability to write to influxdb

---
+++ influxdb.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import os
+
+def write(points, base_url=None, database=None, user=None, password=None):
+  if base_url is None:
+    base_url = os.getenv("INFLUXDB_BASE_URL", "http://localhost:8086")
+  if database is None:
+    database = os.getenv("INFLUXDB_DATABASE", "hookshot")
+  if user is None:
+    user = os.getenv("INFLUXDB_USER", "root")
+  if password is None:
+    password = os.getenv("INFLUXDB_PASSWORD", "root")
+  url = "%s/db/%s/series?u=%s&p=%s" %(base_url, database, user, password)
+  requests.post(url, data=json.dumps(points), headers={
+    "Content-Type": "application/json"
+  })
