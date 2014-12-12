238fc637f89176785d5892c7989f40a106139569
Phips Peter <pspeter333@gmail.com>
2014-12-11 21:06:54 -0800

    Simplifying the ninja file
    There will be multiple dependencies and one hook shot script.

--- ninja.py
+++ ninja.py
@@ -1,47 +1,32 @@
-from influxdb import client as influxdb
 import requests
 import os
 
-DEVICES_URL = "https://api.ninja.is/rest/v0/devices"
-TOKEN_KEY = "user_access_token"
-DATA_KEY = "DA"
-TIMESTAMP_KEY = "timestamp"
-
-TRANSFORMS = {
+transforms = {
   "temperature": lambda x: ((9 * x)/ 5.) + 32
 }
 
-def device_data(req, token, whitelist):
-  params = {}
-  params[TOKEN_KEY] = token
-  res = req.get(DEVICES_URL, params=params)
-  payload = res.json()
+def data(token = None, whitelist = None):
+  if token is None:
+    token = os.getenv("NINJA_TOKEN")
+  if whitelist is None:
+    whitelist = set(os.getenv("NINJA_GUIDS").split(","))
+  response = requests.get("https://api.ninja.is/rest/v0/devices", params={
+    "user_access_token": token
+  })
+  payload = response.json()
   points = []
   for guid, status in payload.get("data", {}).iteritems():
     if guid in whitelist:
       data = status.get("last_data", {})
-      if DATA_KEY in data and TIMESTAMP_KEY in data:
-        type = status.get("device_type", guid)
-        value = data.get(DATA_KEY)
-        timestamp = data.get(TIMESTAMP_KEY)
-        if type in TRANSFORMS:
-          value = TRANSFORMS[type](value)
-        points.append([type, value, timestamp])
-  return points
-
-def main(req, token, whitelist, client):
-  points = device_data(req, token, whitelist)
-  data = {
+      type = status.get("device_type", guid)
+      value = data.get("DA")
+      timestamp = data.get("timestamp") / 1000.
+      if type in transforms:
+        value = transforms[type](value)
+      points.append([timestamp, type, value])
+  return {
     "name": "ninja",
-    "columns": ["type", "value", "time"],
+    "columns": ["time", "type", "value"],
     "points": points
   }
-  client.write_points(data)
-
-if __name__ == '__main__':
-  token = os.environ.get("NINJA_ACCESS_TOKEN")
-  whitelist = set(os.environ.get("NINJA_GUIDS").split(","))
-  client = influxdb.InfluxDBClient(username=os.environ.get("INFLUXDB_USERNAME"),
-    password=os.environ.get("INFLUXDB_PASSWORD"), 
-    database=os.environ.get("INFLUXDB_DB"))
-  main(requests, token, whitelist, client)
+)
